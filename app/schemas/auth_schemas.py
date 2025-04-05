5d389779b32024b983820656866738f87662e09d
Yogesh <yoge.self@gmail.com>
2025-04-05 17:15:51 +0530

    feat(core): set up initial database layer with SQLite and schema definitions

---
+++ app/schemas/auth_schemas.py
@@ -0,0 +1,19 @@
+from tortoise.models import Model
+from tortoise import fields
+import uuid
+
+
+class User(Model):
+    id = fields.UUIDField(primary_key=True, default=lambda: uuid.uuid4(), unique=True)
+    email = fields.CharField(
+        max_length=60, unique=True, null=False, description="email of the user"
+    )
+    password = fields.CharField(
+        max_length=20, null=False, description="password of the user"
+    )
+    username = fields.CharField(
+        max_length=15, unique=True, null=False, description="username of the user"
+    )
+
+    def __str__(self):
+        return self.name
