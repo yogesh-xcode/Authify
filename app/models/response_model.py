b42c1433c69aeb8132d931033319fcba5c6df54c
Yogesh <yoge.self@gmail.com>
2025-04-09 08:09:22 +0530

    feat(models): add response_model for standard API responses

---
+++ app/models/response_model.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+from typing import List, Literal
+
+from app.models.type_model import UserData
+
+
+# Success Response Model
+class BaseDataModel(BaseModel):
+    reason: str
+
+
+class RegisterData(BaseDataModel):
+    user_data: UserData
+
+
+class SuccessResponse(BaseModel):
+    success: Literal[True]
+    message: str
+    data: RegisterData
+
+
+# Error Response Model
+class Ctx(BaseModel):
+    reason: str
+
+
+class ErrorResponse(BaseModel):
+    type: Literal["unauthorized_error", "conflict_error", "integrity_error"]
+    loc: List[str]
+    msg: str
+    input: List[str]
+    ctx: Ctx
