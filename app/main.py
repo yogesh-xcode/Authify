d3e496cf6210425e6203df932ee0524aab21811a
yogesh <yogeshsrilax@gmail.com>
2025-04-20 15:05:06 +0530

    fix(jwt): update main.py, auth routes, and project dependencies according to jwt implementation

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from starlette.middleware.base import BaseHTTPMiddleware
 from app.routes.auth_routes import auth_router
+from app.middlewares.jwt_middleware import jwt_middleware
 from app.config.lifecycle import lifespan
 
 
 app = FastAPI(lifespan=lifespan)
-
+app.add_middleware(BaseHTTPMiddleware, dispatch=jwt_middleware)
 app.include_router(router=auth_router)
